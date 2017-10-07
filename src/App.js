93aa75d26b6b3a98d77f6c191f261d6d80b46913
Andrea Takamiya <takamiya.andrea@gmail.com>
2017-10-06 20:28:05 -1000

    Moving to github.io root page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-
 import NavBar from './components/NavBar';
 import HomepageContainer from './containers/HomepageContainer';
 import WorkContainer from './containers/WorkContainer';
