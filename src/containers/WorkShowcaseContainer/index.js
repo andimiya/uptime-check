4ade42f18a8e3a23f33d1ad79c3e9ed25f077224
Andrea Takamiya <takamiya.andrea@gmail.com>
2017-10-06 19:30:36 -1000

    Images displaying correctly

--- src/containers/WorkShowcaseContainer/index.js
+++ src/containers/WorkShowcaseContainer/index.js
@@ -16,8 +16,8 @@ class WorkShowcaseContainer extends React.Component {
     this.state = {
       content: [],
       selectedContent: {},
+      selectedWorkSample: '',
       urlSlug: '',
-      id: '',
       error: ''
     };
   }
@@ -31,32 +31,38 @@ class WorkShowcaseContainer extends React.Component {
     .then((entry) => {
       this.setState ({ content: entry.items });
       let urlSlug = this.props.location.pathname.split('/').pop();
-      console.log(urlSlug, 'slug');
       const currentlySelected = this.state.content.find((workItem) => workItem.fields.slug === urlSlug);
       if (!currentlySelected){
         this.setState({ error: 'error' })
       } else {
-        this.setState({ selectedContent: currentlySelected.fields })
+        this.setState({
+          selectedContent: currentlySelected.fields,
+          selectedWorkSample: currentlySelected.fields.workSample.fields.file.url
+        })
       }
     })
   }
 
   handleResourceClick(e) {
-    // console.log(e.target.id, 'click id');
     const currentlySelected = this.state.content.find((workItem) => workItem.fields.slug === e.target.id);
     if (!currentlySelected){
       this.setState ({ error: 'error' })
     } else {
-      this.setState({ selectedContent: currentlySelected.fields })
+      this.setState({
+        selectedContent: currentlySelected.fields,
+        selectedWorkSample: currentlySelected.fields.workSample.fields.file.url
+      })
     }
   }
 
   render(props) {
+    console.log(this.state.selectedWorkSample);
     return (
       <div className="work-showcase-container outer">
         <WorkShowcase
           title={this.state.selectedContent.title}
           description={this.state.selectedContent.description}
+          workSample={this.state.selectedWorkSample}
           />
         <WorkContainer
           content={this.state.content}
