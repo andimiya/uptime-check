fa42c9c55857594e11ced8bae5fb689a55045331
Andrea Takamiya <takamiya.andrea@gmail.com>
2017-10-09 17:52:51 -1000

    Responsive hamburger nav finished, could use polish

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,38 +2,61 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 const hamburger = require('../assets/hamburger.svg');
 
-const NavBar = () => {
-  return (
-    <div className="navbar-container">
-      <div className="name-container">
-        <Link to="/">
-          <p>Andrea Takamiya</p>
-        </Link>
-      </div>
-      <div className="menu-item-container">
-        <div className="menu-item">
-          <Link to="/work">
-            <p>Work</p>
+class NavBar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleClick = this.handleClick.bind(this);
+
+    this.state = {
+      navClass: 'navbar-container'
+    };
+  }
+
+  handleClick(){
+    console.log(this.state.navClass);
+    if (this.state.navClass === 'navbar-container') {
+      this.setState ({ navClass: 'responsive-container' })
+    } else {
+      this.setState ({ navClass: 'navbar-container' })
+    }
+  }
+
+  render() {
+    console.log(this.state.navClass);
+
+    return (
+      <div className={this.state.navClass}>
+        <div className="name-container">
+          <Link to="/">
+            <p>Andrea Takamiya</p>
           </Link>
         </div>
-        <div className="menu-item">
-          <Link to="/resume">
-            <p>Resume</p>
-          </Link>
+        <div className="menu-item-container">
+          <div className="menu-item">
+            <Link to="/work">
+              <p>Work</p>
+            </Link>
+          </div>
+          <div className="menu-item">
+            <Link to="/resume">
+              <p>Resume</p>
+            </Link>
+          </div>
+          <div className="menu-item">
+            <Link to="/contact">
+              <p>Contact</p>
+            </Link>
+          </div>
         </div>
-        <div className="menu-item">
-          <Link to="/contact">
-            <p>Contact</p>
-          </Link>
+        <div className="hamburger">
+          <div onClick={this.handleClick}>
+            <img src={hamburger} alt="Work, Resume, Contact" width="40px"/>
+          </div>
         </div>
       </div>
-      <div className="hamburger">
-        <Link to="/work">
-          <img src={hamburger} alt="Work, Resume, Contact" width="40px"/>
-        </Link>
-      </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default NavBar;
