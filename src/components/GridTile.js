036ed941c9e16c2467a798c0dc0d18570c1b2f47
Andrea Takamiya <takamiya.andrea@gmail.com>
2018-04-07 12:24:31 -0700

    Added a few sites, updated css

--- src/components/GridTile.js
+++ src/components/GridTile.js
@@ -1,21 +1,20 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+const siteDown = require('../assets/siteDown.svg');
+const checkmark = require('../assets/checkmark.svg');
 
 const GridTile = props => {
+  
+  const upTime = props.backgroundImage === 'Down' ? siteDown : checkmark;
+  
   return (
     <div className="grid-container-outer">
       <div className="grid-container">
-        <div className="grid-image-block" style={{backgroundImage: `url(${props.workSample})`}}>
-          <Link to={`/work/${props.slug}`} onClick={props.handleResourceClick}>
-            <div id={props.id} className="grid-hover"></div>
-          </Link>
+        <div className="grid-image-block">
+          <img className="status-image" src={upTime} alt="Site Status" />
         </div>
         <div className="grid-subtext">
           <div className="caption-title">
             <p>{props.title}</p>
-          </div>
-          <div className="caption-subtext">
-            <p>{props.position}</p>
           </div>
         </div>
       </div>
--- src/containers/WorkContainer/index.js
+++ src/containers/WorkContainer/index.js
@@ -1,29 +1,102 @@
 import React from 'react';
 import GridTile from '../../components/GridTile';
+import { ajax } from 'jquery';
+import cheerio from 'cheerio';
 
 class WorkContainer extends React.Component {
   constructor(props) {
     super(props);
 
+    this.getSiteSudokrew = this.getSiteSudokrew.bind(this);
+    this.getSiteManomano = this.getSiteManomano.bind(this);
+    this.getSiteCarousel = this.getSiteCarousel.bind(this);
+
+    this.state = {
+      sudokrewUptime: '',
+      manomanoUptime: '',
+      carouselUptime: ''
+    }
   }
 
-  render(props) {
+  componentDidMount() {
+    this.getSiteSudokrew();
+    this.getSiteManomano();
+    this.getSiteCarousel();
+  }
+
+  getSiteSudokrew() {
+    ajax(`http://localhost:8080/api/url/sudokrew.com`).then(body => {
+      let $ = cheerio.load(body);
+      let textSample = $('.navigation__link').text();
+
+      if (!textSample) {
+        this.setState({
+          sudokrewUptime: 'Down'
+        })
+      }
+      else {
+        this.setState({
+          sudokrewUptime: 'Up'
+        })
+      }
+    });
+  }
+
+  getSiteManomano() {
+    ajax(`http://localhost:8080/api/url/manomano.io`).then(body => {
+      let $ = cheerio.load(body);
+      let textSample = $('meta[property="og:title"]').attr('content')
+      
+      if (!textSample) {
+        this.setState({
+          manomanoUptime: 'Down'
+        })
+      }
+      else {
+        this.setState({
+          manomanoUptime: 'Up'
+        })
+      }
+    });
+  }
+
+  getSiteCarousel() {
+    ajax(`http://localhost:8080/api/url/onthecarousel.com`).then(body => {
+      let $ = cheerio.load(body);
+      let textSample = $('.menu__link').text();
+      if (!textSample) {
+        this.setState({
+          carouselUptime: 'Down'
+        })
+      }
+      else {
+        this.setState({
+          carouselUptime: 'Up'
+        })
+      }
+    });
+  }
+
+  render() {
     return (
       <div className="work-container outer">
         <div className="grid-area-container">
           <GridTile
             key="1"
             id="1"
+            backgroundImage={this.state.sudokrewUptime}
             title="Sudokrew.com"
           />
           <GridTile
             key="2"
             id="2"
+            backgroundImage={this.state.manomanoUptime}
             title="manomano.io"
           />
           <GridTile
             key="3"
             id="3"
+            backgroundImage={this.state.carouselUptime}
             title="onthecarousel.com"
           />
         </div>
