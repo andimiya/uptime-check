014ba83d5bae8b312817864104233d8596d45071
Andrea Takamiya <takamiya.andrea@gmail.com>
2018-04-07 15:50:22 -0700

    Made the functions less repetitive

--- src/containers/WorkContainer/index.js
+++ src/containers/WorkContainer/index.js
@@ -1,78 +1,57 @@
-import React from 'react';
-import GridTile from '../../components/GridTile';
-import { ajax } from 'jquery';
-import cheerio from 'cheerio';
+import React from "react";
+import GridTile from "../../components/GridTile";
+import { ajax } from "jquery";
+import cheerio from "cheerio";
 
 class WorkContainer extends React.Component {
   constructor(props) {
     super(props);
 
-    this.getSiteSudokrew = this.getSiteSudokrew.bind(this);
-    this.getSiteManomano = this.getSiteManomano.bind(this);
-    this.getSiteCarousel = this.getSiteCarousel.bind(this);
+    this.siteData = [
+      {
+        url: "sudokrew.com",
+        textSample: `".navigation__link").text()`,
+        stateKey: "sudokrewUptime"
+      },
+      {
+        url: "manomano.io",
+        textSample: `meta[property="og:title"]`,
+        stateKey: "manomanoUptime"
+      },
+      {
+        url: "onthecarousel.com",
+        textSample: `menu__link`,
+        stateKey: "carouselUptime"
+      }
+    ];
+
+    this.getSite = this.getSite.bind(this);
 
     this.state = {
-      sudokrewUptime: '',
-      manomanoUptime: '',
-      carouselUptime: ''
-    }
+      sudokrewUptime: "",
+      manomanoUptime: "",
+      carouselUptime: ""
+    };
   }
 
   componentDidMount() {
-    this.getSiteSudokrew();
-    this.getSiteManomano();
-    this.getSiteCarousel();
-  }
-
-  getSiteSudokrew() {
-    ajax(`http://localhost:8080/api/url/sudokrew.com`).then(body => {
-      let $ = cheerio.load(body);
-      let textSample = $('.navigation__link').text();
-
-      if (!textSample) {
-        this.setState({
-          sudokrewUptime: 'Down'
-        })
-      }
-      else {
-        this.setState({
-          sudokrewUptime: 'Up'
-        })
-      }
+    this.siteData.map(site => {
+      return this.getSite(site);
     });
   }
 
-  getSiteManomano() {
-    ajax(`http://localhost:8080/api/url/manomano.io`).then(body => {
+  getSite(site) {
+    ajax(`http://localhost:8080/api/url/${site.url}`).then(body => {
       let $ = cheerio.load(body);
-      let textSample = $('meta[property="og:title"]').attr('content')
-      
+      let textSample = $`(${site.textSample}`;
       if (!textSample) {
         this.setState({
-          manomanoUptime: 'Down'
-        })
-      }
-      else {
+          [site.stateKey]: "Down"
+        });
+      } else {
         this.setState({
-          manomanoUptime: 'Up'
-        })
-      }
-    });
-  }
-
-  getSiteCarousel() {
-    ajax(`http://localhost:8080/api/url/onthecarousel.com`).then(body => {
-      let $ = cheerio.load(body);
-      let textSample = $('.menu__link').text();
-      if (!textSample) {
-        this.setState({
-          carouselUptime: 'Down'
-        })
-      }
-      else {
-        this.setState({
-          carouselUptime: 'Up'
-        })
+          [site.stateKey]: "Up"
+        });
       }
     });
   }
@@ -81,28 +60,19 @@ class WorkContainer extends React.Component {
     return (
       <div className="work-container outer">
         <div className="grid-area-container">
-          <GridTile
-            key="1"
-            id="1"
-            backgroundImage={this.state.sudokrewUptime}
-            title="Sudokrew.com"
-          />
-          <GridTile
-            key="2"
-            id="2"
-            backgroundImage={this.state.manomanoUptime}
-            title="manomano.io"
-          />
-          <GridTile
-            key="3"
-            id="3"
-            backgroundImage={this.state.carouselUptime}
-            title="onthecarousel.com"
-          />
+          {this.siteData.map(site => {
+            return (
+              <GridTile
+                key={`${site.url}`}
+                backgroundImage={`this.state.${site.stateKey}`}
+                title={`${site.url}`}
+              />
+            );
+          })}
         </div>
       </div>
-    )
-  };
-};
+    );
+  }
+}
 
 export default WorkContainer;
